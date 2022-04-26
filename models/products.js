d5a11c838a8f25b69cfd615689fc3f3bad3d0037
Inusah Said <saidinusah29@gmail.com>
2022-04-26 19:34:51 +0000

    added timestamps

--- models/products.js
+++ models/products.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-const product = new mongoose.Schema({
-	name: { type: String, required: true },
-	title: { type: String, required: true },
-	picture: { type: String, required: true },
-	price: { type: Number, required: true },
-});
+const product = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		title: { type: String, required: true },
+		picture: { type: String, required: true },
+		price: { type: Number, required: true },
+	},
+	{ timestamps: true }
+);
 
 const Product = mongoose.model('product', product);
 module.exports = { Product };

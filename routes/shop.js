f6c314d004bd091fdfd2431a9d12a1208456334b
Inusah Said <saidinusah29@gmail.com>
2022-05-23 23:39:29 +0000

    what?

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authenticate = require('../middlewares/authenticate');
-const ProductController = require('../controllers/productsController');
+ const ProductController = require('../controllers/productsController');
 const ProductControllerInstance = new ProductController();
-router.get('/', [authenticate], ProductControllerInstance.showAllProducts);
+router.get('/', ProductControllerInstance.showAllProducts);
 
 module.exports = router;
